f4e3a1e2ac248b6bb9c94dd1a6e0eaee6223848d
Ferdi Emmen <fjemmen@gmail.com>
2017-11-07 21:55:33 +0100

    Add debounce to Input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,36 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import debounce from 'lodash.debounce';
 
+/** Enhanced input element with callbacks and debounce. */
 class Input extends React.Component {
 
-  handleOnChange = event => {
-    event.persist();
+  constructor(props) {
+    super(props);
+    this.handleOnChange = debounce(this.handleOnChange, (props.debounceDelay && props.debounceDelay));
+  }
+
+  handleOnChange(event) {
+    event.persist();  
+
+    // Trigger onEmpty hook because input field has been cleared.
+    if (!event.target.value && Object.prototype.hasOwnProperty.call(this.props, 'onEmpty')) {
+      this.props.onEmpty();
+      return false;
+    };
+
+    // Trigger onChange hook returning current value.
     this.props.onChange(event.target.value);
   }
 
   render() {
     return (
       <div className="input">
-        <input type="text" onChange={this.handleOnChange} />
+        <input type="text" onChange={event => {
+          event.persist();
+          this.handleOnChange(event);
+        }} />
       </div>
     )
   }
@@ -20,7 +38,15 @@ class Input extends React.Component {
 
 Input.propTypes = {
   /** Callback function on changes */
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  /** Callback function on an empty value */
+  onEmpty: PropTypes.func,
+  /** Delay before callback function is invoked */
+  debounceDelay: PropTypes.number,
 };
+
+Input.defaultProps = {
+  debounceDelay: 100,
+}
 
 export default Input;
--- src/docs/examples/Input/ExampleInput.js
+++ src/docs/examples/Input/ExampleInput.js
@@ -4,7 +4,7 @@ import Input from 'ps-react/Input';
 
 /** Check console for returned value */
 export default function ExampleInput() {
-  return <Input onChange={doSomething} />
+  return <Input onChange={doSomething} debounceDelay={500} />
 }
 
 function doSomething(value) {
