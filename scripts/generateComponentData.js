af09ffe76a0627b46bda4d65a008b3ea0baca77c
ferdiemmen <fjemmen@gmail.com>
2017-11-07 13:39:36 +0100

    Add autogenerated documentation

--- scripts/generateComponentData.js
+++ scripts/generateComponentData.js
@@ -1,3 +1,4 @@
+
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
@@ -10,7 +11,7 @@ const paths = {
   output: path.join(__dirname, '../config', 'componentData.js')
 };
 
-const enableWatchMode = process.argv.slice(2) === '--watch';
+const enableWatchMode = process.argv.slice(2) == '--watch';
 if (enableWatchMode) {
   // Regenerate component metadata when components or examples change.
   chokidar.watch([paths.examples, paths.components]).on('change', (event, path) => generate(paths));
@@ -19,7 +20,7 @@ if (enableWatchMode) {
   generate(paths);
 }
 
-const generate = (paths) => {
+function generate(paths) {
   const errors = [];
   const componentData = getDirectories(paths.components).map(componentName => {
     try {
@@ -32,8 +33,8 @@ const generate = (paths) => {
   writeFile(paths.output, 'module.exports = ' + JSON.stringify(errors.length ? errors : componentData));
 }
 
-const getComponentData = (paths, componentName) => {
-  const content = readFile(path.join(paths.component, componentName, componentName + '.js'));
+function getComponentData(paths, componentName) {
+  const content = readFile(path.join(paths.components, componentName, componentName + '.js'));
   const info = parse(content);
   return {
     name: componentName,
@@ -44,7 +45,7 @@ const getComponentData = (paths, componentName) => {
   }
 }
 
-const getExampleData = (examplesPath, componentName) => {
+function getExampleData(examplesPath, componentName) {
   const examples = getExampleFiles(examplesPath, componentName);
   return examples.map(file => {
     const filePath = path.join(examplesPath, componentName, file);
@@ -58,4 +59,32 @@ const getExampleData = (examplesPath, componentName) => {
       code: content
     }
   });
+}
+
+function getExampleFiles(examplesPath, componentName) {
+  let exampleFiles = [];
+  try {
+    exampleFiles = getFiles(path.join(examplesPath, componentName));
+  } catch(error) {
+    console.log(chalk.red(`No examples found for ${componentName}.`));
+  }
+  return exampleFiles;
+}
+
+function getDirectories(filepath) {
+  return fs.readdirSync(filepath).filter(file => fs.statSync(path.join(filepath, file)).isDirectory());
+}
+
+function getFiles(filepath) {
+  return fs.readdirSync(filepath).filter(file => fs.statSync(path.join(filepath, file).isFile()));
+}
+
+function writeFile(filepath, content) {
+  fs.writeFile(filepath, content, (err) => {
+    err ? console.log(chalk.red(err)) : console.log(chalk.green('Component data saved.'));
+  });
+}
+
+function readFile(filePath) {
+  return fs.readFileSync(filePath, 'utf-8');
 }
